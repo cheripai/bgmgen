adca8b42a76e0427025e073f2f723efc9639ca8a
dat <datdo@rocketmail.com>
2017-07-30 22:41:54 -0700

    Use fc2 layer of VGG

--- data/compile_dataset.py
+++ data/compile_dataset.py
@@ -2,23 +2,27 @@ import bcolz
 import numpy as np
 import os
 import sys
-from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.models import Model
+from keras.preprocessing import image
 from random import shuffle
+
+MAX_LENGTH = 16384
 
 
 def img2feat(img, model):
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    feat = model.predict(x)
+    return model.predict(x)
 
 
 if __name__ == "__main__":
     img_dir = sys.argv[1]
     pianoroll_dir = sys.argv[2]
 
-    model = VGG16(include_top=False, weights="imagenet")
+    base_model = VGG16(include_top=True, weights="imagenet")
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer("fc2").output)
 
     img_paths = []
 
@@ -29,25 +33,23 @@ if __name__ == "__main__":
 
     pianoroll_paths = [img_path.replace(img_dir, pianoroll_dir, 1).replace(".jpg", ".bcolz") for img_path in img_paths]
 
-    feats = np.zeros((len(img_paths), 2048))
-    for i, img_path in enumerate(img_paths):
+    N = len(img_paths)
+
+    index_shuffled = list(range(N))
+    shuffle(index_shuffled)
+
+    feats = np.zeros((N, 4096))
+    for img_path, i in zip(img_paths, index_shuffled):
         img = image.load_img(img_path, target_size=(224, 224))
         feats[i] = img2feat(img, model)
     
-    pianorolls = []
-    for pianoroll_path in pianoroll_paths:
+    pianorolls = np.zeros((N, MAX_LENGTH, 128))
+    for pianoroll_path, i in zip(pianoroll_paths, index_shuffled):
         pianoroll = bcolz.open(pianoroll_path)[:]
-        pianorolls.append(pianoroll)
+        pianoroll_length = min(MAX_LENGTH, pianoroll.shape[0])
+        pianorolls[i,:pianoroll_length,:] = pianoroll[:pianoroll_length,:]
 
-    index_shuffled = list(range(len(pianorolls)))
-    shuffle(index_shuffled)
-    feats_shuffled = np.zeros((len(pianorolls), 2048))
-    pianorolls_shuffled = []
-    for i, index in enumerate(index_shuffled):
-        feats_shuffled[i] = feats[index]
-        pianorolls_shuffled.append(pianorolls[index])
-
-    c = bcolz.carray(feats_shuffled, rootdir="img_feats.bcolz", mode="w")
+    c = bcolz.carray(feats, rootdir="img_feats.bcolz", mode="w")
     c.flush()
-    c = bcolz.carray(pianorolls_shuffled, rootdir="pianorolls.bcolz", mode="w")
+    c = bcolz.carray(pianorolls, rootdir="pianorolls.bcolz", mode="w")
     c.flush()
