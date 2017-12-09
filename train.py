21e337e47e8ccf0b51254e03ea09259beb197159
dat <datdo@rocketmail.com>
2017-12-08 18:24:51 -0800

    Finish simple training loop

--- train.py
+++ train.py
@@ -5,10 +5,12 @@ import torch
 from models.network import Generator
 from torch.autograd import Variable
 
+from tqdm import tqdm
+
 IMG_FEAT_SIZE = 4096
 OUTPUT_SIZE = 128
 HIDDEN_SIZE = 512
-END_SONG_TOKEN = -1
+epochs = 1
 
 use_cuda = torch.cuda.is_available()
 
@@ -16,11 +18,10 @@ use_cuda = torch.cuda.is_available()
 def train(x, y, generator, opt, criterion, teacher_forcing_ratio=0.5, train=False):
     opt.zero_grad()
 
-    batch_size = x.size(0)
+    batch_size = 1
     hidden = generator.init_hidden(batch_size)
     hidden = hidden.cuda() if use_cuda else hidden
     max_length = y.size(1)
-    print(max_length)
 
     if train:
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
@@ -35,24 +36,26 @@ def train(x, y, generator, opt, criterion, teacher_forcing_ratio=0.5, train=Fals
 
     if use_teacher_forcing:
         for i in range(max_length):
+            if y[:, i].sum().data[0] < 0:
+                break
             output, hidden = generator(generator_input, x, hidden)
             loss += criterion(output, y[:, i])
-            generator_input = y[:, i]
+            generator_input[:] = y[:, i]
 
     else:
         for i in range(max_length):
+            if y[:, i].sum().data[0] < 0:
+                break
             output, hidden = generator(generator_input, x, hidden)
             loss += criterion(output, y[:, i])
-
-           # generator_input = Variable(torch.LongTensor(batch_size, 1))
-           # generator_input = generator_input.cuda() if use_cuda else generator_input
             generator_input[:] = output
 
     if train:
         loss.backward()
         opt.step()
 
-    return loss.data[0] / target_length
+    return loss.data[0] / i
+
 
 if __name__ == "__main__":
     imgs = bcolz.open(sys.argv[1])
@@ -61,22 +64,32 @@ if __name__ == "__main__":
     generator = Generator(IMG_FEAT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
     optimizer = torch.optim.Adam(generator.parameters(), lr=0.001)
     criterion = torch.nn.MultiLabelMarginLoss()
-    
+
     if use_cuda:
         generator = generator.cuda()
         criterion = criterion.cuda()
 
-    for img, pianoroll in zip(imgs, pianorolls):
-        img = Variable(torch.from_numpy(img).type(torch.FloatTensor))
-        pianoroll = Variable(torch.from_numpy(pianoroll).type(torch.LongTensor))
+    N = len(imgs)
+    for e in range(epochs):
+        shuffled_indexes = list(range(N))
+        random.shuffle(shuffled_indexes)
+        epoch_loss = 0
+        for i in tqdm(shuffled_indexes):
+            img, pianoroll = imgs[i], pianorolls[i]
+            img = Variable(torch.from_numpy(img).type(torch.FloatTensor))
+            pianoroll = Variable(torch.from_numpy(pianoroll).type(torch.LongTensor))
 
-        if use_cuda:
-            img = img.cuda()
-            pianoroll = pianoroll.cuda()
+            if use_cuda:
+                img = img.cuda()
+                pianoroll = pianoroll.cuda()
 
-        # TEMP
-        img = img.unsqueeze(0)
-        pianoroll = pianoroll.unsqueeze(0)
+            # TEMP
+            img = img.unsqueeze(0)
+            pianoroll = pianoroll.unsqueeze(0)
 
-        loss = train(img, pianoroll, generator, optimizer, criterion, train=True)
-        print(loss)
+            epoch_loss += train(img, pianoroll, generator, optimizer, criterion, train=True)
+        print("Epoch {}: {}".format(e, epoch_loss / N))
+
+        torch.save(generator.state_dict(), "data/weights.pth")
+
+        
