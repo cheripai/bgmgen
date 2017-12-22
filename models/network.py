bc7c56008c51763768b005043295a8a2a7e8b830
dat <datdo@rocketmail.com>
2017-12-22 14:38:38 -0800

    Add encoder network.

--- models/network.py
+++ models/network.py
@@ -1,18 +1,52 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 
+class Encoder(nn.Module):
+    def __init__(self, img_feat_size, dropout_p=0.1):
+        super(Encoder, self).__init__()
+        self.dropout_p = dropout_p
+
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=2)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=2)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=2)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(8*8*256, 1024)
+        self.bn6 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, img_feat_size)
+
+        self.dropout = nn.Dropout(self.dropout_p)
+
+    def forward(self, x):
+        x = F.max_pool2d(self.bn1(F.relu(self.conv1(x))), (2, 2))
+        x = F.max_pool2d(self.bn2(F.relu(self.conv2(x))), (2, 2))
+        x = F.max_pool2d(self.bn3(F.relu(self.conv3(x))), (2, 2))
+        x = F.max_pool2d(self.bn4(F.relu(self.conv4(x))), (2, 2))
+        x = F.max_pool2d(self.bn5(F.relu(self.conv5(x))), (2, 2))
+        x = x.view(-1, 8*8*256)
+        x = self.dropout(self.bn6(F.relu(self.fc1(x))))
+        x = self.fc2(x)
+        return x
+        
+
 class Generator(nn.Module):
-    def __init__(self, img_feat_size, hidden_size, output_size, n_layers=1, dropout_p=0.1, max_length=16384):
+    def __init__(self, img_feat_size, hidden_size, output_size, n_layers=1, dropout_p=0.1):
         super(Generator, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.n_layers = n_layers
         self.dropout_p = dropout_p
-        self.max_length = max_length
 
-        self.linear = nn.Linear(img_feat_size + output_size, hidden_size)
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.linear = nn.Linear(img_feat_size + self.hidden_size, hidden_size)
         self.gru = nn.GRU(
             self.hidden_size,
             self.hidden_size,
@@ -20,14 +54,17 @@ class Generator(nn.Module):
             dropout=self.dropout_p)
         self.out = nn.Linear(self.hidden_size, self.output_size)
         self.dropout = nn.Dropout(self.dropout_p)
-        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, input, img_feat, hidden):
-        input_combined = torch.cat((input, img_feat), 1)
-        output = self.dropout(self.linear(input_combined))
+        embedded = self.embedding(input).squeeze(1)
+        embedded = self.dropout(embedded)
+        output = embedded.unsqueeze(0)
+        combined = torch.cat((embedded, img_feat), 1)
+        output = self.dropout(self.linear(combined))
         output = output.unsqueeze(0)
         output, hidden = self.gru(output, hidden)
-        output = self.sigmoid(self.out(output.squeeze(0)))
+        output = self.softmax(self.out(output.squeeze(0)))
         return output, hidden
 
     def init_hidden(self, batch_size):
@@ -35,18 +72,10 @@ class Generator(nn.Module):
 
 
 if __name__ == "__main__":
-    hidden_size = 512
-    output_size = 128
-    img_feat_size = 4096
-    batch = 4
-    model = Generator(img_feat_size, hidden_size, output_size)
+    batch_size = 4
+    img_feat_size = 512
 
-    x = Variable(torch.zeros(batch, output_size))
-    y = Variable(torch.zeros(batch, img_feat_size))
-    hidden = model.init_hidden(batch)
-    
-    output, hidden = model(x, y, hidden)
-    for i in range(10):
-        output, hidden = model(output, y, hidden)
+    model = Encoder(img_feat_size)
 
-    print(output.size(), hidden.size())
+    img = Variable(torch.zeros((batch_size, 3, 224, 224)))
+    model(img)
