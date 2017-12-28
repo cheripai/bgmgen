7b23fcdd8f517edf4a1c0a32b66af5bc21588053
dat <datdo@rocketmail.com>
2017-12-27 22:14:56 -0800

    Use pretty_midi to preprocess

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,47 +1,30 @@
-import bcolz
-import json
 import numpy as np
 import os
+import pretty_midi as pm
 import sys
-from midi2json import replace_root
-from pianoroll import Pianoroll
+from melody import Melody
+
+MELODY_INDEX = 0
 
 
-def preprocess(json):
-    processed = {}
-    try:
-        i = 0
-        while len(json["tracks"][i]["notes"]) == 0: i += 1
-        processed["notes"] = json["tracks"][i]["notes"]
-        processed["header"] = json["header"]
-        return processed
-    except:
-        return None
+def replace_extension(name, ext):
+    base = "".join(name.split(".")[:-1])
+    if "." in ext:
+        return base + ext
+    else:
+        return base + "." + ext
 
 
 if __name__ == "__main__":
     source_dir = sys.argv[1]
     target_dir = sys.argv[2]
 
-    sources, targets = [], []
-    for root, dirs, files in os.walk(source_dir):
-        for d in dirs:
-            target_d = os.path.join(replace_root(root, target_dir), d)
-            if not os.path.exists(target_d):
-                os.makedirs(target_d)
-        for f in files:
-            if f.endswith(".json"):
-                sources.append(os.path.join(root, f))
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
 
-    for source in sources:
-        target = source.replace(".json", ".bcolz")
-        target = replace_root(target, target_dir)
-        targets.append(target)
-
-    for source, target in zip(sources, targets):
-        with open(source) as f:
-            song = json.load(f)
-
-        processed = preprocess(song)
-        pianoroll = Pianoroll(processed["header"]["bpm"], processed["notes"])
-        bcolz.carray(pianoroll.roll, rootdir=target).flush()
+    for midi_file in os.listdir(source_dir):
+        if midi_file.endswith("midi") or midi_file.endswith("mid"):
+            midi_data = pm.PrettyMIDI(os.path.join(source_dir, midi_file), resolution=8)
+            print(midi_data.resolution)
+            melody = Melody(midi_data.instruments[MELODY_INDEX].get_piano_roll())
+            np.save(os.path.join(target_dir, replace_extension(midi_file, "npy").lower().replace("'", "")), melody.sequence)
