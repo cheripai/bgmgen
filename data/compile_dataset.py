311cec7de71c70a89f9247f29e01d815ee6003e7
dat <datdo@rocketmail.com>
2017-12-31 16:16:37 -0800

    Preprocess as sequence of tokens from pianoroll

--- data/compile_dataset.py
+++ data/compile_dataset.py
@@ -1,24 +1,23 @@
 import numpy as np
 import os
+import pickle
 import sys
 from PIL import Image
 from random import shuffle
 
-MAX_LENGTH = 2048
-END_SONG_TOKEN = 130
 IMG_SIZE = 224
 
 def get_filename(fname):
-    return fname.split("/")[-1].split(".")[0]
+    return fname.split("/")[-1].split(".")[0].lower()
 
 if __name__ == "__main__":
     bg_path = sys.argv[1]
-    sequence_path = sys.argv[2]
+    pianoroll_path = sys.argv[2]
 
-    sequence_paths = [os.path.join(sequence_path, path) for path in os.listdir(sequence_path) if path.endswith("npy")]
-    bg_paths = [os.path.join(bg_path, get_filename(path).split("-")[0].strip()+".jpg") for path in sequence_paths]
+    pianoroll_paths = [os.path.join(pianoroll_path, path) for path in os.listdir(pianoroll_path) if path.endswith("npy")]
+    bg_paths = [os.path.join(bg_path, get_filename(path).split("-")[0].strip()+".jpg") for path in pianoroll_paths]
 
-    bgs = np.zeros((len(sequence_paths), 3, IMG_SIZE, IMG_SIZE))
+    bgs = np.zeros((len(pianoroll_paths), 3, IMG_SIZE, IMG_SIZE))
     for i, bg_path in enumerate(bg_paths):
         bg = Image.open(bg_path)
         bg = bg.resize((IMG_SIZE, IMG_SIZE))
@@ -27,13 +26,24 @@ if __name__ == "__main__":
         bgs[i] = bg
 
 
-    sequences = np.zeros((len(sequence_paths), MAX_LENGTH))
-    for i, sequence_path in enumerate(sequence_paths):
-        sequence = np.load(sequence_path)
-        sequence_length = min(MAX_LENGTH, sequence.shape[0])
-        sequences[i,:sequence_length] = sequence[:sequence_length]
-        sequences[i,sequence_length:] = END_SONG_TOKEN
-        sequences[i,sequence_length-1] = END_SONG_TOKEN
+    songs = []
+    tokens = []
+    for i, pianoroll_path in enumerate(pianoroll_paths):
+        pianoroll = np.load(pianoroll_path)
+        song = np.zeros(pianoroll.shape[0])
+        for j, step in enumerate(pianoroll):
+            notes = np.where(step == 1)[0]
+            if len(notes) > 3:
+                notes = sorted(np.random.choice(notes, 3, replace=False))
+            token = str(list(notes))
+            if token not in tokens:
+                tokens.append(token)
+            song[j] = tokens.index(token)
+        songs.append(song)
 
+    tokens.append("SOS")
+
+    print("{} songs with {} unique tokens".format(len(songs), len(tokens)))
     np.save("bgs.npy", bgs)
-    np.save("sequences.npy", sequences)
+    pickle.dump(songs, open("songs.pkl", "wb"))
+    pickle.dump(tokens, open("tokens.pkl", "wb"))
--- data/preprocess.py
+++ data/preprocess.py
@@ -3,8 +3,10 @@ import os
 import pretty_midi as pm
 import sys
 from melody import Melody
+from pianoroll import to_piano_roll
 
 MELODY_INDEX = 0
+RESOLUTION = 8
 
 
 def replace_extension(name, ext):
@@ -13,7 +15,6 @@ def replace_extension(name, ext):
         return base + ext
     else:
         return base + "." + ext
-
 
 if __name__ == "__main__":
     source_dir = sys.argv[1]
@@ -24,7 +25,8 @@ if __name__ == "__main__":
 
     for midi_file in os.listdir(source_dir):
         if midi_file.endswith("midi") or midi_file.endswith("mid"):
-            midi_data = pm.PrettyMIDI(os.path.join(source_dir, midi_file), resolution=8)
-            print(midi_data.resolution)
-            melody = Melody(midi_data.instruments[MELODY_INDEX].get_piano_roll())
-            np.save(os.path.join(target_dir, replace_extension(midi_file, "npy").lower().replace("'", "")), melody.sequence)
+            midi_data = pm.PrettyMIDI(os.path.join(source_dir, midi_file))
+            # melody = Melody(midi_data.instruments[MELODY_INDEX].get_piano_roll(), midi_data.resolution)
+            del midi_data.instruments[1]
+            pianoroll = to_piano_roll(midi_data)
+            np.save(os.path.join(target_dir, replace_extension(midi_file, "npy").lower().replace("'", "")), pianoroll)
